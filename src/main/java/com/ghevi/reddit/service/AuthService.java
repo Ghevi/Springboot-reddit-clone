6fafb39f97ee5640218cce22d7df7300b520a44c
Ghevi <ghevi96@gmail.com>
2020-08-17 02:31:46 +0200

    ÃìFixed thymeleaf dependency

--- src/main/java/com/ghevi/reddit/service/AuthService.java
+++ src/main/java/com/ghevi/reddit/service/AuthService.java
@@ -41,7 +41,7 @@ public class AuthService {
         userRepository.save(user);
         String token = generateVerificationToken(user);
 
-        mailService.sendMail(new NotificationEmail("Please activate you Account",
+        mailService.sendMail(new NotificationEmail("Please, activate your Account",
                 user.getEmail(), "Thank you for signing up to Spring Reddit, " +
                 "please, click on the url below to activate your account: " +
                 "http://localhost:8080/api/auth/accountVerification/" + token));
